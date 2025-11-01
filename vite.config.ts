195c2317505d6aaa23f2cc17dda86704c211e1b6
lukkshh <xlukkass@gmail.com>
2025-11-01 21:24:40 +0400

    add svg loader, add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-
+import svgLoader from "vite-svg-loader";
+import path from "path";
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [vue(), tailwindcss(), svgLoader()],
 });
