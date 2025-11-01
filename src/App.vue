51ae2ea38b10d22184a6c6894dd2c5ef2ad345a7
lukkshh <xlukkass@gmail.com>
2025-11-01 22:10:36 +0400

    add Header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GlobalNotification from "./components/GlobalNotification.vue";
+import Header from "./components/Header.vue";
+</script>
 
 <template>
+  <GlobalNotification />
+  <Header></Header>
+  <div class="h-screen bg-[#F2F0F1]"></div>
   <router-view />
 </template>
