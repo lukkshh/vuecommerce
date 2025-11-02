ce5aff2afe692d63944b6d9ff13efe79e1fcfbcb
lukkshh <xlukkass@gmail.com>
2025-11-02 13:14:54 +0400

    make home page, add brands

---
+++ src/components/Brands.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import versace from "@/assets/images/versace.svg";
+import zara from "@/assets/images/zara.svg";
+import gucci from "@/assets/images/gucci.svg";
+import prada from "@/assets/images/prada.svg";
+import calvin_klein from "@/assets/images/calvin-klein.svg";
+</script>
+
+<template>
+  <section
+    class="bg-black h-[120px] flex flex-wrap gap-x-2 md:gap-x-30 items-center justify-center p-4"
+  >
+    <versace alt="Versace" class="scale-50 md:scale-100" />
+    <zara alt="Zara" class="scale-50 md:scale-100" />
+    <gucci alt="Gucci" class="scale-50 md:scale-100" />
+    <prada alt="Prada" class="scale-50 md:scale-100" />
+    <calvin_klein alt="Calvin Klein" class="scale-50 md:scale-100" />
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,58 @@
 <script setup lang="ts">
 import People from "@/assets/images/main-people.svg?url";
+import Star from "@/assets/images/star.svg";
+
+import Brands from "../components/Brands.vue";
 </script>
 
 <template>
-  <section class="bg-[#F2F0F1]">
-    <div class="flex flex-col justify-center items-center p-4">
-      <h1 class="font-integral text-4xl">
+  <section class="bg-[#F2F0F1] flex flex-col md:flex-row">
+    <div
+      class="flex flex-col justify-center items-center lg:ml-20 md:items-start p-4"
+    >
+      <h1 class="font-integral text-4xl md:text-6xl">
         FIND CLOTHES THAT MATCHES YOUR STYLE
       </h1>
-      <p class="opacity-60 mt-5">
+      <p class="opacity-60 mt-5 md:max-w-[700px]">
         Browse through our diverse range of meticulously crafted garments,
         designed to bring out your individuality and cater to your sense of
         style.
       </p>
       <button
-        class="mt-6 cursor-pointer rounded-[62px] h-[52px] w-[95%] bg-black text-white"
+        class="mt-6 cursor-pointer rounded-[62px] h-[52px] w-[95%] md:w-[210px] bg-black text-white"
       >
         Shop Now
       </button>
       <div
-        class="mt-5 flex justify-center flex-wrap items-center gap-x-12 gap-y-3 relative"
+        class="mt-5 flex justify-center flex-wrap items-center gap-x-12 md:gap-x-10 gap-y-3 relative"
       >
         <div class="flex flex-col">
-          <span class="font-bold text-2xl">200+</span>
+          <span class="font-bold text-2xl lg:text-[40px]">200+</span>
           <span class="opacity-60 text-xs">International Brand</span>
         </div>
 
         <div class="w-px h-10 bg-black opacity-10"></div>
 
         <div class="flex flex-col">
-          <span class="font-bold text-2xl">2000+</span>
+          <span class="font-bold text-2xl lg:text-[40px]">2000+</span>
           <span class="opacity-60 text-xs">High-Quality Products</span>
         </div>
 
+        <div class="hidden sm:block w-px h-10 bg-black opacity-10"></div>
+
         <div class="flex flex-col">
-          <span class="font-bold text-2xl">30,000+</span>
+          <span class="font-bold text-2xl lg:text-[40px]">30,000+</span>
           <span class="opacity-60 text-xs">Happy Customers</span>
         </div>
       </div>
     </div>
-    <div class="w-full overflow-hidden flex justify-center items-center">
-      <img class="w-full" :src="People" alt="People" />
+    <div
+      class="w-full relative overflow-hidden flex justify-center items-center"
+    >
+      <Star class="absolute scale-50 top-25 left-3 2xl:left-50" />
+      <Star class="absolute scale-80 top-5 right-5 2xl:top-10 2xl:right-30" />
+      <img class="w-full lg:w-[60%]" :src="People" alt="People" />
     </div>
   </section>
+  <Brands></Brands>
 </template>
